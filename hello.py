5c74b2eb9c4dfe788e95870611d9ce578b841d0d
emilyobrien17 <e6obrien@student.bridgew.edu>
2018-01-20 14:21:30 -0500

    Updated for submission.

--- hello.py
+++ hello.py
@@ -1,6 +1,4 @@
 print("Hello, World!")
 print("PyCharm is fun")
-print("What's Your Middle Name?")
-midName = input()
-# input("Press <Enter>")
-print("Your Middle Name is: {0}".format(midName))
+input("Press <Enter>")
+
